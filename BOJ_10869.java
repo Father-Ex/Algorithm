beab1b3c5b35fc22db47bc2b85970e1484df5609
Hungry <noinjurum@gmail.com>
2022-01-11 19:16:01 +0900

    10869 완료

---
+++ BOJ_10869.java
@@ -0,0 +1,16 @@
+package baek;
+
+import java.util.Scanner;
+
+public class BOJ_10869 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
