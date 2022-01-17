b646269613b96dd437dbe82be8bc24eddcad7bda
Hungry <noinjurum@gmail.com>
2022-01-17 10:03:53 +0900

    3003 성능 개선

--- BOJ_3003.java
+++ BOJ_3003.java
@@ -6,10 +6,10 @@ public class BOJ_3003 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int[] rightNums = {1, 1, 2, 2, 2, 8};
-        int tmp = 0;
+        StringBuilder sb = new StringBuilder();
         for(int i=0; i<rightNums.length; i++){
-            tmp = scan.nextInt();
-            System.out.print(rightNums[i]-tmp+" ");
+            sb.append(rightNums[i]-scan.nextInt()+" ");
         }
+        System.out.println(sb.toString());
     }
 }
