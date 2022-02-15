e0c73a5eb4a7c8be3528235f5548ede725289fd4
Hungry <noinjurum@gmail.com>
2022-02-15 18:12:07 +0900

    다음주 문제 README 추가, 2447 완료

---
+++ others/BOJ_2447.java
@@ -0,0 +1,46 @@
+package baek.others;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ_2447 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int length = Integer.parseInt(reader.readLine());
+
+        printStar(length);
+    }
+    public static void printStar(int length){
+        String[] stars = makeStar(length);
+        
+        for(String tmp:stars){
+            System.out.println(tmp);
+        }
+    }
+    public static String[] makeStar(int length){
+        if(length==3){
+            String[] s = {"***","* *","***"};
+            return s;
+        }else{
+            String[] s = new String[length];
+            String[] ss = makeStar(length/3);
+            int idx = 0;
+            
+            for(String tmp:ss){
+                s[idx++] = tmp+tmp+tmp;
+            }
+            String blank = "";
+            for(int i=0; i<length/3; i++){
+                blank += " ";
+            }
+            for(String tmp:ss){
+                s[idx++] = tmp+blank+tmp;
+            }
+            for(String tmp:ss){
+                s[idx++] = tmp+tmp+tmp;
+            }
+
+            return s;
+        }
+    }
+}
